ce11405f20d8c20393bea57a3bd5983be08f117b
Olaf Faaland <faaland1@llnl.gov>
2021-10-26 15:27:19 -0700

    make whitespace consistent

--- getres.c
+++ getres.c
@@ -3,7 +3,7 @@
 #include <flux/core.h>
 
 struct lookup_ctx {
-	char *resource;
+    char *resource;
 };
 
 void lookup_continuation (flux_future_t *f, void *arg)
@@ -14,7 +14,7 @@ void lookup_continuation (flux_future_t *f, void *arg)
 
     if (flux_kvs_lookup_get (f, &value) < 0) {
         perror("flux_kvs_lookup_get failed");
-	return;
+        return;
     }
 
     ctx->resource = strdup(value);
@@ -24,51 +24,54 @@ void lookup_continuation (flux_future_t *f, void *arg)
 
 char * fetch_resource_string()
 {
-	flux_t *h;
-	flux_future_t *f;
-	flux_reactor_t *r;
-	char *ns = NULL;
-	struct lookup_ctx ctx = {0};
-	const char *key = "resource.R";
+    flux_t *h;
+    flux_future_t *f;
+    flux_reactor_t *r;
+    char *ns = NULL;
+    struct lookup_ctx ctx = {0};
+    const char *key = "resource.R";
 
-	if (!(h = flux_open(NULL, 0))) {
-		perror("flux_open failed");
-		exit(1);
-	}
+    if (!(h = flux_open(NULL, 0))) {
+        perror("flux_open failed");
+        exit(1);
+    }
 
-	if (!(f = flux_kvs_lookup(h, ns, 0, key))) {
-		perror("flux_kvs_lookup failed");
-		exit(2);
-	}
+    if (!(f = flux_kvs_lookup(h, ns, 0, key))) {
+        perror("flux_kvs_lookup failed");
+        exit(2);
+    }
 
-	if (flux_future_then (f, -1., lookup_continuation, &ctx) < 0) {
-		perror("flux_future_then failed");
-		exit(3);
-	}
+    if (flux_future_then (f, -1., lookup_continuation, &ctx) < 0) {
+        perror("flux_future_then failed");
+        exit(3);
+    }
 
-	if (!(r = flux_get_reactor(h))) {
-		perror ("flux_get_reactor failed");
-		exit(4);
-	}
+    if (!(r = flux_get_reactor(h))) {
+        perror ("flux_get_reactor failed");
+        exit(4);
+    }
 
-	if (flux_reactor_run(r, 0) < 0) {
-		perror ("flux_reactor_run failed");
-		exit(5);
-	}
+    if (flux_reactor_run(r, 0) < 0) {
+        perror ("flux_reactor_run failed");
+        exit(5);
+    }
 
-	flux_close(h);
+    flux_close(h);
 
-	return ctx.resource;
-
+    return ctx.resource;
 }
 
 int main(int argc, char **argv)
 {
-	char *res;
+    char *res;
 
-	res = fetch_resource_string();
-	printf("resource is %s\n", res);
-	free(res);
+    res = fetch_resource_string();
+    printf("resource is %s\n", res);
+    free(res);
 
-	return(0);
+    return(0);
 }
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
