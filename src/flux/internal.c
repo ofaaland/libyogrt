ca2743cee1f1b17b008395a117637c84f2e4e5c4
Olaf Faaland <faaland1@llnl.gov>
2021-12-13 10:48:50 -0800

    use jobid for init
    It's unclear which attribute I can count on to fetch.

--- src/flux/internal.c
+++ src/flux/internal.c
@@ -37,20 +37,9 @@ int verbosity = 0;
 int internal_init(int verb)
 {
 	verbosity = verb;
-    flux_t *h = NULL;
-    char *state;
 
-    if (!(h = flux_open(NULL, 0))) {
-        error("ERROR: flux_open() failed. Are you running under flux?\n"
-              " Remaining time will be a bogus value.\n");
-        return 0;
-    }
-
-    state = flux_attr_get(h, "state");
-    flux_close(h);
-
-    if (!state) {
-        error("ERROR: flux_attr_get() failed. Are you running under flux?\n"
+	if (!getenv("FLUX_JOB_ID")) {
+        debug("ERROR: FLUX_JOB_ID is not set."
               " Remaining time will be a bogus value.\n");
         return 0;
     }
